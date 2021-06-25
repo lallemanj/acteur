5665ec39b9485ebc61895a84e337c150dced1d2f
lallemanj <julien.lalleman@gmail.com>
2021-06-25 11:49:28 +0200

    maincontroller

--- src/main/java/ehb/festival/acteur/controllers/MainController.java
+++ src/main/java/ehb/festival/acteur/controllers/MainController.java
@@ -2,10 +2,14 @@ package ehb.festival.acteur.controllers;
 
 import ehb.festival.acteur.dao.ActeurDAO;
 import ehb.festival.acteur.dao.FestivalDAO;
+import ehb.festival.acteur.entities.Acteur;
 import ehb.festival.acteur.entities.Festival;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -23,5 +27,19 @@ public class MainController {
     @ResponseBody
     public Iterable<Festival> giveAllFestivals(){
         return mFestivalDAO.findAll();
+    }
+
+    @PostMapping("/acteur/new")
+    @ResponseBody
+    public HttpStatus createActeur (@RequestParam(value = "naam")String naam,
+                                    @RequestParam(value = "leeftijd")int leeftijd,
+                                    @RequestParam(value = "prijs")double prijs){
+
+        Acteur temp = new Acteur();
+        temp.setLeeftijd(leeftijd);
+        temp.setNaam(naam);
+        temp.setPrijs(prijs);
+        mActeurDAO.save(temp);
+        return HttpStatus.OK;
     }
 }
