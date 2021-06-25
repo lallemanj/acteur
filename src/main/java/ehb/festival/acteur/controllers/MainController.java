df028ca2fea3076583c235c4e7086d1658602b61
lallemanj <julien.lalleman@gmail.com>
2021-06-25 11:37:28 +0200

    add ingevulde MainController half + properties

--- src/main/java/ehb/festival/acteur/controllers/MainController.java
+++ src/main/java/ehb/festival/acteur/controllers/MainController.java
@@ -1,4 +1,27 @@
 package ehb.festival.acteur.controllers;
 
+import ehb.festival.acteur.dao.ActeurDAO;
+import ehb.festival.acteur.dao.FestivalDAO;
+import ehb.festival.acteur.entities.Festival;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class MainController {
+    private ActeurDAO mActeurDAO;
+    private FestivalDAO mFestivalDAO;
+
+    @Autowired
+    public MainController(ActeurDAO mActeurDAO, FestivalDAO mFestivalDAO) {
+        this.mActeurDAO = mActeurDAO;
+        this.mFestivalDAO = mFestivalDAO;
+    }
+
+    @GetMapping("/festivals")
+    @ResponseBody
+    public Iterable<Festival> giveAllFestivals(){
+        return mFestivalDAO.findAll();
+    }
 }
